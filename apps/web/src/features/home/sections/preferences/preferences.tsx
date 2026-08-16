4465aa838ddc203626a97c68fefb916ce9dbc645
Adryel Bezerra <adryelrbrz+github@gmail.com>
2026-08-16 21:15:33 +0100

    fix(web): correct z-index layers for components

--- apps/web/src/features/home/sections/preferences/preferences.tsx
+++ apps/web/src/features/home/sections/preferences/preferences.tsx
@@ -17,24 +17,22 @@ import { ThemeTabs, LanguageTabs } from './components';
 
 export function Preferences() {
     return (
-        <aside>
-            <Popover>
-                <PopoverTrigger
-                    aria-label='preferences'
-                    className='absolute right-5 bottom-5'
-                    render={<Button variant='outline' size='icon-lg' />}
-                >
-                    <SettingsIcon className='size-5 stroke-[1.5]' />
-                </PopoverTrigger>
-                <PopoverContent aria-label='preferences' className='mr-5'>
-                    <PopoverHeader>
-                        <PopoverTitle>Preferences</PopoverTitle>
-                        <PopoverDescription>Change theme or language.</PopoverDescription>
-                    </PopoverHeader>
-                    <ThemeTabs />
-                    <LanguageTabs />
-                </PopoverContent>
-            </Popover>
-        </aside>
+        <Popover>
+            <PopoverTrigger
+                aria-label='preferences'
+                className='fixed right-5 bottom-5 z-10'
+                render={<Button variant='outline' size='icon-lg' />}
+            >
+                <SettingsIcon className='size-5 stroke-[1.5]' />
+            </PopoverTrigger>
+            <PopoverContent aria-label='preferences' className='mr-5'>
+                <PopoverHeader>
+                    <PopoverTitle>Preferences</PopoverTitle>
+                    <PopoverDescription>Change theme or language.</PopoverDescription>
+                </PopoverHeader>
+                <ThemeTabs />
+                <LanguageTabs />
+            </PopoverContent>
+        </Popover>
     );
 }
