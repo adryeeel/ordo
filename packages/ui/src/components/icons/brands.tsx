d40fd82eae601e66d51469b608c8c4effcdc94ab
Adryel Bezerra <adryelrbrz+github@gmail.com>
2026-08-26 20:13:22 +0100

    feat(ui): add icons for brands, flags and symbols

---
+++ packages/ui/src/components/icons/brands.tsx
@@ -0,0 +1,4 @@
+import { SiInstagram, SiX } from '@icons-pack/react-simple-icons';
+
+export const XIcon = SiX;
+export const InstagramIcon = SiInstagram;
---
+++ packages/ui/src/components/icons/flags.tsx
@@ -0,0 +1,4 @@
+import { US, PT } from 'country-flag-icons/react/3x2';
+
+export const USFlagIcon = US;
+export const PTFlagIcon = PT;
